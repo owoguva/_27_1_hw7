c284724ec66d44a7d4def6ae90eb14c0cd772960
owoguva <125554270+owoguva@users.noreply.github.com>
2023-03-23 00:31:52 +0600

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,7 @@
 from aiogram import types, Dispatcher
 from config import ADMINS, bot
-
+from database.bot_dp import sql_command_all, sql_command_delete
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 async def ban(message: types.Message):
@@ -19,6 +20,28 @@ async def ban(message: types.Message):
     else:
         await message.answer("Пиши в группе!")
 
+async def delete_data(message: types.Message):
+    if message.from_user.id not in ADMINS:
+        await message.answer("ТЫ НЕ МОЙ БОСС!")
+    else:
+        users = await sql_command_all()
+        for user in users:
+            await message.answer(
+                f"@{user[1]} {user[2]} {user[3]} {user[4]}\n"
+                f"{user[5]}",
+                reply_markup=InlineKeyboardMarkup().add(
+                    InlineKeyboardButton(f"DELETE {user[2]}",
+                                         callback_data=f"DELETE {user[0]}")
+                )
+            )
+
+async def complete_delete(call: types.CallbackQuery):
+    await sql_command_delete(call.data.replace("DELETE ", ""))
+    await call.answer(text="УДАЛЕНО!", show_alert=True)
+    await call.message.delete()
 
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(ban, commands=['ban'], commands_prefix='!/')
+    dp.register_message_handler(delete_data, commands=['del'])
+    dp.register_callback_query_handler(complete_delete,
+                                       lambda call: call.data and call.data.startswith
