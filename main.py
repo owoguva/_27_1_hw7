d775540a79d45deff650f31b3cf8385344f60712
owoguva <125554270+owoguva@users.noreply.github.com>
2023-03-23 00:31:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from aiogram.utils import executor
-from config import dp
+from config import dp, bot, ADMINS
 from handlers import client, callback, admin, extra, fsmAdminMentor
 import logging
+from database.bot_dp import sql_create
+
+async def on_startup(_):
+    await bot.send_message(ADMINS, "Я родился!")
+    sql_create()
+
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
+fsmAdminMentor.register_handlers_fsm_anketa(dp)
+
 
 extra.register_handlers_extra(dp)
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
