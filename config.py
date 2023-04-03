7ca72d4d57a42d19a75b9afa99d9e4440eb852f0
owoguva <125554270+owoguva@users.noreply.github.com>
2023-04-03 14:33:49 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from decouple import config
+
+
+storage = MemoryStorage()
 
 TOKEN = config("TOKEN")
 
 bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
 ADMINS = (890463711)
+OPENAI_TOKEN="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"
