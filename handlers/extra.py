97469db69386f55637f7a51330be6f9a3f68d0a5
owoguva <125554270+owoguva@users.noreply.github.com>
2023-04-06 23:54:59 +0600

    Update extra.py

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,36 +1,22 @@
-from aiogram import types, Dispatcher
-import random
-from config import OPENAI_TOKEN
 import openai
+from aiogram import types, Dispatcher
+
+from config import OPENAI_TOKEN
+
 
 openai.api_key = OPENAI_TOKEN
 async def bad_words_filter(message: types.Message):
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="The following is a conversation with an AI assistant. "
-               "The assistant is helpful, creative, clever, and very friendly."
-               "\n\nHuman: Hello, who are you?\nAI: "
-               "I am an AI created by OpenAI."
-               " How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"]
-    )
-    await message.answer(response['choice'][0]['text'])
-
-
-
-    if message.text.startswith('game'):
-        animated_emojis = ['🎯','🎳','⚽️','🏀','🎰','🎲']
-        random_emoji = random.choice(animated_emojis)
-        await message.answer(random_emoji)
-
-
-
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=message.text,
+            temperature=0.9,
+            max_tokens=1000,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.6,
+        )
+        await message.answer(response['choices'][0]['text'])
 
 def register_handlers_extra(dp: Dispatcher):
-    dp.register_message_handler(bad_words_filter, content_types=['text'])
+    dp.register_message_handler(bad_words_filter,commands=['go'], content_types=['text'])
